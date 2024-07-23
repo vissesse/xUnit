e5e3ec4ad06d31927f266077fbe1e0003cbe2eca
carlos vissesse <carloscarlycsony@gmail.com>
2024-07-23 18:37:57 +0100

    igonpre files

--- src/TestCase.py
+++ src/TestCase.py
@@ -2,24 +2,23 @@ from TestResult import TestResult
 
 
 class TestCase:
-    def __init__(self, name:str):
-        self.name=name
-        
-    def run(self): 
+    def __init__(self, name: str):
+        self.name = name
+
+    def run(self):
         result = TestResult()
         result.testStarted()
         self.setUp()
         try:
-            method=getattr(self, self.name)
+            method = getattr(self, self.name)
             method()
         except:
             result.testFailed()
         self.tearDown()
         return result
-    
+
     def setUp(self):
         pass
-    
+
     def tearDown(self):
         pass
-
